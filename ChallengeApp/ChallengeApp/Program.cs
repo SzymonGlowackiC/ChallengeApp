6e33da7087e87604152c444aeb3b7bc07a8ca8ec
Szymon Głowacki <szymonglowacki13.02@gmail.com>
2023-02-28 19:40:28 +0100

    zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,16 @@
 ﻿var name = "Szymon";
 var sex = "Mężczyzna";
-var age = 21;
+var age = 17;
 
-if (sex == "Mężczyzna") 
+if (sex == "Mężczyzna" && age < 18) 
 {
-    if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-    else if (age > 18)
-    {
-        Console.WriteLine("Pełnoletni mężczyzna");
-    }
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
