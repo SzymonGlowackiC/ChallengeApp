564f4f84ef5ac8df72ea9438f4f4170b1ce76f8e
SzymonGlowackiC <Szymonglowacki13.02@gmail.com>
2023-09-14 18:39:51 +0200

    dzień 12

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,9 +1,15 @@
 ﻿
+using System.Diagnostics;
+
 namespace ChallengeApp
 {
     public class Employee
     {
         private List<float> grades = new List<float>();
+
+        public Employee()
+        {
+        }
         public Employee(string name, string surname)
         {
             this.Name = name;
@@ -55,6 +61,33 @@ namespace ChallengeApp
             float gradeAsFloat = (float)grade;
             this.AddGrade(gradeAsFloat);
         }
+        public void AddGrade(char grade)
+        {
+            switch(grade)
+            {
+                case 'A':
+                case 'a':
+                    this.grades.Add(100);
+                    break;
+                case 'B':
+                    this.grades.Add(80);
+                    break;
+                case 'C':
+                    this.grades.Add(60);
+                    break;
+                case 'D':
+                    this.grades.Add(40);
+                    break;
+                case 'E':
+                    this.grades.Add(20);
+                    break;
+                default:
+                    Console.WriteLine("Wrong letter");
+                    break;
+            }
+            //float gradeAsFloat = (float)grade;
+            //this.AddGrade(gradeAsFloat);
+        }
 
         public Statistics GetStatistics()
         {
@@ -75,6 +108,26 @@ namespace ChallengeApp
             }
 
             statistics.Average = statistics.Average / this.grades.Count;
+
+            switch(statistics.Average)
+            {
+                case var average when average >= 80:
+                    statistics.AverageLetter = 'A';
+                    break;
+                case var average when average >= 60:
+                    statistics.AverageLetter = 'B';
+                    break;
+                case var average when average >= 40:
+                    statistics.AverageLetter = 'C';
+                    break;
+                case var average when average >= 20:
+                    statistics.AverageLetter = 'D';
+                    break;
+                default:
+                    statistics.AverageLetter = 'E';
+                    break;
+            }
+
             return statistics;
         }
 
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,14 +1,25 @@
 ﻿using ChallengeApp;
 
-var employee = new Employee("Adam", "Kamilzelich");
-employee.AddGrade("Adam");
-employee.AddGrade("2");
-employee.AddGrade(5);
-employee.AddGrade(600);
-employee.AddGrade(6);
+Console.WriteLine("Witamy w programie do oceny pracowników ");
+Console.WriteLine("=======================================");
+Console.WriteLine();
+
+
+var employee = new Employee();
+
+while (true)
+{
+    Console.WriteLine("Podaj kolejną ocenę pracownika:  ");
+    var input = Console.ReadLine();
+    if(input == "q")
+    {
+        break;
+    }
+    employee.AddGrade(input);
+}
+
 var statistics = employee.GetStatistics();
-
-
-Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"AVG: { statistics.Average}");
+Console.WriteLine($"AVG Letter: {statistics.AverageLetter}");
 Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max: {statistics.Max}");
